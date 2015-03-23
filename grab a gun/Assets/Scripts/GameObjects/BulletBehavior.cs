8c64bc2837670068b54e4731f2e71e34aedfe9fe
41407 <jimi.welling@cs.helsinki.fi>
2015-03-23 12:22:27 +0200

    Add bullet, basic scripts, prefabs

---
+++ grab a gun/Assets/Scripts/GameObjects/BulletBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	void Update () {
+		transform.Translate (transform.rotation * Vector3.up * speed * Time.deltaTime);
+	}
+}
---
+++ grab a gun/Assets/Scripts/GameObjects/Health.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+		public int health;
+		public int currentHealth;
+		public int healthVariance;
+		public bool applyScoring = true;
+		public GameObject explosion;
+		public GameObject tookDamage;
+		public bool pooledObject = true;
+
+		void OnEnable ()
+		{
+				if (explosion) {
+						ObjectPool.pool.Initialize (explosion);
+				}
+				if (tookDamage) {
+						ObjectPool.pool.Initialize (tookDamage);
+				}
+				health = Mathf.Max (1, health + Random.Range (-healthVariance, healthVariance));
+				currentHealth = health;
+		}
+
+		public void TakeDamage (int damage)
+		{
+				currentHealth -= damage;
+				if (tookDamage) {
+//						Instantiate (tookDamage, transform.position, Quaternion.identity);
+						ObjectPool.pool.Pull (tookDamage, transform.position, transform.rotation).SetActive (true);
+		
+				}
+				CheckHealth ();
+		}
+	
+		void CheckHealth ()
+		{
+				if (currentHealth <= 0) {
+						gameObject.SendMessage ("ZeroHealth");
+						if (applyScoring) {
+								gameObject.SendMessage ("ApplyScoring");
+						}
+						if (explosion) {
+//								Instantiate (explosion, transform.position, Quaternion.identity);
+								ObjectPool.pool.Pull (explosion, transform.position, transform.rotation).SetActive (true);
+		
+						}
+						if (pooledObject) {
+								gameObject.SetActive (false);
+						} else {
+								Destroy (gameObject);		
+						}
+				}
+		}
+}
