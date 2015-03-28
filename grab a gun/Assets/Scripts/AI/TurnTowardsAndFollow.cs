abbfca78c69e1a286b92281e3f6167dca9b8a768
41407 <jimi.welling@cs.helsinki.fi>
2015-03-28 13:45:00 +0200

    Tweak gfx, add sprites, movable enemies

---
+++ grab a gun/Assets/Scripts/AI/TurnTowardsAndFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnTowardsAndFollow : MonoBehaviour
+{
+		public GameObject player;
+		public float turningSpeed;
+		public float moveSpeed;
+	
+		void Update ()
+		{
+				if (player) {
+						TurnTowardsPlayer ();
+				}
+				GetComponent<Rigidbody2D>().AddForce (transform.rotation * Vector2.up * moveSpeed * Time.deltaTime);
+		}
+
+		public void SetPlayer (GameObject player)
+		{
+				this.player = player;
+		}
+
+		void TurnTowardsPlayer ()
+		{
+				Vector2 heading = player.transform.position - transform.position;
+				float angle = transform.rotation.eulerAngles.z;
+				float angleBetween = Vector2.Angle (Vector2.up, heading);
+				if (heading.x > 0) {
+						angleBetween = 360 - angleBetween;
+				}
+				if (angleBetween - angle > 180) {
+						angleBetween -= 360;	
+				}
+		
+				if (angleBetween - angle < -180) {
+						angleBetween += 360;	
+				}
+				if (angleBetween - angle > 0) {
+						gameObject.transform.Rotate (new Vector3 (0, 0, turningSpeed * Time.deltaTime));
+				} else {
+						gameObject.transform.Rotate (new Vector3 (0, 0, -turningSpeed * Time.deltaTime));
+				}	
+		}
+}
--- grab a gun/Assets/Scripts/Graphics/SetCutoutSprite.cs
+++ grab a gun/Assets/Scripts/Graphics/SetCutoutSprite.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SetCutoutSprite : MonoBehaviour {
-
+public class SetCutoutSprite : MonoBehaviour
+{
+	
 	public Texture sprite;
+	public Texture normalMap;
 
 	// Use this for initialization
-	void OnEnable () {
-		GetComponent<Renderer> ().material.mainTexture = sprite;	
+	void OnEnable ()
+	{
+		GetComponent<Renderer> ().material.mainTexture = sprite;
+		if (normalMap) {
+			GetComponent<Renderer> ().material.SetTexture ("_BumpMap", normalMap);
+		}
 	}
 }
