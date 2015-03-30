558d6d6dd969b5a0fa2398884fb4ea0e940c8e7c
41407 <jimi.welling@cs.helsinki.fi>
2015-03-30 15:30:27 +0300

    Pooling particles

---
+++ grab a gun/Assets/Scripts/Object Initialisation/ParticleDecay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDecay : MonoBehaviour
+{
+	void Update ()
+	{
+		if (gameObject.GetComponent<ParticleSystem> ().isStopped) {
+			transform.parent.gameObject.SetActive (false);
+		}
+	}
+
+	void OnEnable ()
+	{
+		gameObject.GetComponent<ParticleSystem> ().Play ();
+	}
+	
+	void OnDisable ()
+	{
+		gameObject.GetComponent<ParticleSystem> ().Stop ();
+	}
+}
