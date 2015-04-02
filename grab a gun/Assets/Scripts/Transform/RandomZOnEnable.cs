812abd03713faee815a22b9d004039e667c81884
41407 <jimi.welling@cs.helsinki.fi>
2015-04-02 17:36:21 +0300

    Beautifuler spatters, new model

---
+++ grab a gun/Assets/Scripts/Transform/RandomZOnEnable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomZOnEnable : MonoBehaviour
+{
+
+	public float min;
+	public float max;
+
+	void OnEnable ()
+	{
+		Vector3 position = transform.position;
+		position.z = Random.Range (min, max);
+		transform.position = position;
+	}
+}
