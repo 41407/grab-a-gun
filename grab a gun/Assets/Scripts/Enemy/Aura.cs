0a44903ef3a60ff9db7a4184ee734060c3c0f824
41407 <jimi.welling@cs.helsinki.fi>
2015-04-10 22:08:52 +0300

    Add Aura script

---
+++ grab a gun/Assets/Scripts/Enemy/Aura.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aura : MonoBehaviour
+{
+	public bool emitsAura = false;
+	public AuraType activeAura;
+	public int level = 0;
+
+	void OnEnable ()
+	{
+		emitsAura = false;
+	}
+
+	void EnableAura ()
+	{
+		emitsAura = true;
+		activeAura = SetAuraByLevel ();
+	}
+
+	void SetLevel (int newLevel)
+	{
+		level = newLevel;
+	}
+
+	private AuraType SetAuraByLevel ()
+	{
+		return AuraType.Speed;
+	}
+}
